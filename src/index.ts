b53c8c51362c76686c1e8827bc7ba0cde4352d4b
Scott Piriou <30843220+pscott@users.noreply.github.com>
2024-10-29 14:16:24 +0100

    implement verification function

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,42 @@
 #!/usr/bin/env ts-node
 
-import { poseidonHashMany } from 'micro-starknet';
-import { CallData, cairo, selector } from 'starknet';
 import * as fs from 'fs';
 import * as path from 'path';
+import { clients } from '@snapshot-labs/sx';
+import { RpcProvider } from 'starknet';
+import { starknetSepolia } from '@snapshot-labs/sx';
+const { EthereumTx } = clients;
+
+// These values doesn't matter for this script, but it's required to instantiate the EthereumTx client
+const starkProvider = new RpcProvider({
+    nodeUrl: 'http://127.0.0.1:5050/rpc'
+  });
+const ethUrl = 'http://127.0.0.1:8545';
+
+let ethTxClient = new EthereumTx({
+    starkProvider: starkProvider as any,
+    ethUrl,
+    networkConfig: starknetSepolia,
+});
 
 // Helper function to load data from data.json
-function loadData() {
+function loadJson() {
     const filePath = path.resolve(__dirname, '../data.json');
     const rawData = fs.readFileSync(filePath, 'utf-8');
     return JSON.parse(rawData);
 }
 
-// Helper function to compute vote commit
-function computeVoteCommit(data: any) {
-    const vote = data;
-
-    const voteCommitPreImage = CallData.compile({
-        target: data.space.address,
-        selector: selector.getSelectorFromName('vote'),
-        ...vote
-    });
-
-    return `0x${poseidonHashMany(voteCommitPreImage.map((v: string) => BigInt(v))).toString(16)}`;
-}
-
-// Helper function to compute proposal commit
-function computeProposalCommit(data: any) {
-    const proposal = {
-        author: data.signer.address,
-        metadataUri: ['0x1', '0x4'],
-        executionStrategy: {
-            address: '0x0000000000000000000000000000000000005678',
-            params: ['0x0'],
-        },
-        userProposalValidationParams: [
-            '0xffffffffffffffffffffffffffffffffffffffffff',
-            '0x1234',
-            '0x5678',
-            '0x9abc',
-        ]
-    };
-
-    const proposeCommitPreImage = CallData.compile({
-        target: data.space.address,
-        selector: selector.getSelectorFromName('propose'),
-        ...proposal
-    });
-
-    return `0x${poseidonHashMany(proposeCommitPreImage.map((v: string) => BigInt(v))).toString(16)}`;
-}
-
-// Helper function to compute update proposal commit
-function computeUpdateProposalCommit(data: any) {
-    const updateProposal = {
-        author: data.signer.address,
-        proposalId: cairo.uint256('0x1'),
-        executionStrategy: {
-            address: '0x0000000000000000000000000000000000005678',
-            params: ['0x0'],
-        },
-        metadataUri: ['0x1', '0x5']
-    };
-
-    const updateCommitPreImage = CallData.compile({
-        target: data.space.address,
-        selector: selector.getSelectorFromName('update_proposal'),
-        ...updateProposal
-    });
-
-    return `0x${poseidonHashMany(updateCommitPreImage.map((v: string) => BigInt(v))).toString(16)}`;
-}
-
-// Main function
-function verify(type: string, expectedHash: string) {
-    const data = loadData();
-    let computedHash = '';
+async function verify(expectedHash: string) {
+    const json = loadJson().result;
+    let computedHash;
+    const type = json.type.toLowerCase();
 
     if (type === 'vote') {
-        computedHash = computeVoteCommit(data);
+        computedHash = await ethTxClient.getVoteHash(json.sender, json.data);
     } else if (type === 'proposal') {
-        computedHash = computeProposalCommit(data);
-    } else if (type === 'updateProposal') {
-        computedHash = computeUpdateProposalCommit(data);
+        computedHash = await ethTxClient.getProposeHash(json.sender, json.data);
+    } else if (type === 'updateproposal') {
+        computedHash = await ethTxClient.getUpdateProposalHash(json.sender, json.data);
     } else {
         console.error('Invalid type specified. Use "vote", "proposal", or "updateProposal".');
         process.exit(1);
@@ -98,16 +52,11 @@ function verify(type: string, expectedHash: string) {
 // Parse CLI arguments
 const args = process.argv.slice(2);
 
-if (args.length < 2) {
-    console.error('Error: Missing arguments. Usage: yarn verify [vote | proposal | updateProposal] expectedHash');
+if (args.length < 1) {
+    console.error('Error: Missing arguments. Usage: yarn verify <expectedHash> [FILE]');
     process.exit(1);
 }
 
-const [type, expectedHash] = args;
+const expectedHash = args[0];
 
-if (!expectedHash) {
-    console.error('Error: expectedHash is required.');
-    process.exit(1);
-}
-
-verify(type!, expectedHash);
+verify(expectedHash!);
